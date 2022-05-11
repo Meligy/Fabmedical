2268445b6058d876d7f7e184f1d87a011edf2fe7
Meligy <mohamed.meligy@purple.telstra.com>
2022-05-11 04:45:51 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("5dd8296a-b197-41fa-80e8-52e6bfd5b21f");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
